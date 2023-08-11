bd4327b6df1906b42202a1712c3f830272bc9cc6
shashi <sthan011@ucr.edu>
2023-08-11 14:09:02 -0700

    My First Commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+
+<title>My Blog</title>
+
+<body>
+
+    <article>
+
+        <a href="/post"><h1>First Post</h1></a>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget convallis ligula. Nullam auctor bibendum ligula, ac commodo nisi tristique in. Suspendisse potenti. Nulla facilisi. Nunc euismod pellentesque est, non cursus dui ultricies at. Aenean euismod volutpat diam, eget laoreet nisi consectetur ut. Quisque malesuada purus nec ex sollicitudin, eget pulvinar ligula dignissim. Proin in nulla arcu. Fusce eu ligula non libero pulvinar laoreet. Mauris tristique justo eget semper sodales. Vivamus ac nisl eu odio dapibus elementum. Curabitur interdum nunc sit amet nisl volutpat, at pulvinar ipsum vehicula. Integer consectetur, mauris sit amet imperdiet venenatis, libero nunc tristique tellus, et posuere justo risus sit amet nisi. Nam euismod turpis nec odio suscipit, nec blandit justo bibendum. Sed dignissim bibendum elit vel cursus. Fusce vitae augue in ex varius pellentesque. Aliquam erat volutpat. Sed nec nunc id arcu laoreet sagittis.</p>
+
+    </article>
+
+    <a href="/">Go Back</a>
+
+
+</body>
